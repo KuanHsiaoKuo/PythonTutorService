926c8da70522ee9a5d4ebccbb84e8265a338a21f
KuanHsiaoKuo <kuanhsiaokuo@gmail.com>
2022-06-30 16:52:51 +0800

    reformat

--- src/bottle_server.py
+++ src/bottle_server.py
@@ -12,10 +12,11 @@
 # pip install bottle
 
 from bottle import route, get, request, run, template, static_file
+
 try:
-    import StringIO # NB: don't use cStringIO since it doesn't support unicode!!!
+    import StringIO  # NB: don't use cStringIO since it doesn't support unicode!!!
 except:
-    import io as StringIO # py3
+    import io as StringIO  # py3
 import json
 import pg_logger
 
@@ -30,6 +31,7 @@ import pg_logger
 def dummy_ok(name=None):
     return 'OK'
 
+
 @route('/<filepath:path>')
 def index(filepath):
     return static_file(filepath, root='.')
@@ -43,22 +45,24 @@ def index(filepath):
 @route('/LIVE_exec_py2.py')
 @route('/LIVE_exec_py3.py')
 def get_py_exec():
-  out_s = StringIO.StringIO()
+    out_s = StringIO.StringIO()
 
-  def json_finalizer(input_code, output_trace):
-    ret = dict(code=input_code, trace=output_trace)
-    json_output = json.dumps(ret, indent=None)
-    out_s.write(json_output)
+    def json_finalizer(input_code, output_trace):
+        ret = dict(code=input_code, trace=output_trace)
+        json_output = json.dumps(ret, indent=None)
+        out_s.write(json_output)
 
-  options = json.loads(request.query.options_json)
+    options = json.loads(request.query.options_json)
 
-  pg_logger.exec_script_str_local(request.query.user_script,
-                                  request.query.raw_input_json,
-                                  options['cumulative_mode'],
-                                  options['heap_primitives'],
-                                  json_finalizer)
+    pg_logger.exec_script_str_local(
+        request.query.user_script,
+        request.query.raw_input_json,
+        options['cumulative_mode'],
+        options['heap_primitives'],
+        json_finalizer
+    )
 
-  return out_s.getvalue()
+    return out_s.getvalue()
 
 
 if __name__ == "__main__":
